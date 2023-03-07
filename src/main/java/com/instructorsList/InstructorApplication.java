c1df7767dab0bdf93e529b02b0e6eba27093b741
= <=>
2023-03-06 23:21:25 -0500

    project creation

---
+++ src/main/java/com/instructorsList/InstructorApplication.java
@@ -0,0 +1,13 @@
+package com.instructorsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instructorsList/InstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instructorsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
